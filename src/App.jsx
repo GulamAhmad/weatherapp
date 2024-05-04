d144d40934b17429dc83c5d7cf8ef0d4238d002d
GulamAhmad <ahmedgulam83@gmail.com>
2024-05-04 13:35:56 +0530

    turbo shared button

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,28 @@ import "./App.css";
 import { IoMdRefreshCircle } from "react-icons/io";
 import logo from "./assets/sunwireless.svg";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Button from "../../../packages/shared/Button";
 
 function App() {
   return (
     <div className="app">
       <header>
+        <Button>Click</Button>
         <div>
           <div className="logo">
-            <img src={logo} alt="sun with waves"/> <h3>Weather 99</h3>
+            <img src={logo} alt="sun with waves" /> <h3>Weather 99</h3>
           </div>
-          <button className="btn" onClick={()=>  window.location.reload()}>
+          <button className="btn" onClick={() => window.location.reload()}>
             {" "}
             <span>
               <IoMdRefreshCircle />
-            </span> Refresh
+            </span>{" "}
+            Refresh
           </button>
         </div>
       </header>
       <main>
-        <Dashboard/>
+        <Dashboard />
       </main>
     </div>
   );
